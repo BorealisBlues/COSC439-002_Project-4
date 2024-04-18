041d5f17eb7bcaf0af52e2cd5ac71846d8d083f1
BorealisBlues <Andromedaweir@gmail.com>
2024-04-18 13:21:43 -0400

    Add Main.py & TicTacToe.py

---
+++ Main.py
@@ -0,0 +1,12 @@
+# Main Driver File - Initial Commit - Andromeda
+
+def main():
+    # Driver code goes here
+    pass 
+
+
+
+
+
+if (__name__ == "__main__"):
+    main()
---
+++ TicTacToe.py
@@ -0,0 +1,8 @@
+#Class definition for internal game logic
+
+class TicTacToe: #god i dont rember how classes in python work i gotta look it up
+    
+    
+    def __init__():
+        pass
+    
