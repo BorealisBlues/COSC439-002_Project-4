e41dd013db7f99fd2a0903d45cb56ce764dd7507
b-price <bprice12@students.towson.edu>
2024-04-23 23:09:44 -0400

    Connected GUI with logic. Some weirdness with late game moves

--- Main.py
+++ Main.py
@@ -1,12 +1,13 @@
 # Main Driver File - Initial Commit - Andromeda
+from TicTacToe import TicTacToe
+from TicTacToeGUI import TicTacToeBoard
+
 
 def main():
-    # Driver code goes here
-    pass 
+    game = TicTacToe()
+    board = TicTacToeBoard(game)
+    board.mainloop()
 
 
-
-
-
-if (__name__ == "__main__"):
+if __name__ == "__main__":
     main()
