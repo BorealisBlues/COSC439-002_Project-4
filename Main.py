a739f6791279ef549f50bda95d1d1fd65ec48c7d
BorealisBlues <Andromedaweir@gmail.com>
2024-04-29 12:46:43 -0400

    Added TODO list to Main.py in comments should have been in last commit but i forgor

--- Main.py
+++ Main.py
@@ -4,6 +4,13 @@ from TicTacToeGUI import TicTacToeBoard
 
 
 def main():
+    ##  TODO:
+    ##      - Add a screen for Create Game (host server) / Join Game (run client)
+    ##      - impliment netcode into GUI
+    ##      - TEST the netcode and make sure we dont have weird bugs in it
+    ##      - Fix Bug in TicTacToe.py : Diagonal win checking only procs on \ direction not / direction
+    
+    
     game = TicTacToe()
     board = TicTacToeBoard(game)
     board.mainloop()
